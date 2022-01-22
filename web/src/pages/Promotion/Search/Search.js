51cbe22b5cf46ad828339bd0c7f2622ffc619b0d
Francisco José <francisco.jsantana3@gmail.com>
2022-01-22 18:55:04 -0300

    Comunicando com API REST

---
+++ web/src/pages/Promotion/Search/Search.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import PromotionCard from 'components/Promotion/Card/Card';
+import axios from 'axios';
+
+const PagesPromotionSearch = () => {
+
+  const [promotions, setPromotions] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/promotions?_embed=comments')
+    .then((response) =>{
+      setPromotions(response.data);
+    });
+  }, []);
+ 
+  return (
+    <div 
+       style={{
+        maxWidth: 800,
+        margin: '30px auto',
+      }}
+      >
+        {promotions.map((promotion) => (
+          <PromotionCard promotion={promotion} />
+        ))}
+      </div>
+  );
+};
+
+export default PagesPromotionSearch;
